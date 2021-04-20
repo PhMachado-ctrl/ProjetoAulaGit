a3c2eb8873335c27cdcc728f4ef4602082d87c95
Pedro Henrique Machado Bastos <phmachado@gmail.com.br>
2021-04-20 08:36:37 -0300

    Atualização do css

---
+++ OneDrive/Documentos/Faculdade/LTPW II/PROJECT-CRUD/js/prodlist.js
@@ -0,0 +1,36 @@
+
+
+//Referencia ao DOM HTML
+const tbodyList = document.getElementById('tbodyList');
+
+//Código
+const api = axios.create({
+    baseURL: 'http://18.224.8.119:3334/'
+})
+
+function listAll(){
+    console.log('Consulta de dados....');
+
+    api.get('produtos').then(res=>{
+        console.log('Realizando a consulta ....');
+        const data = res.data;
+        let i, tr;
+    
+
+        for(i=0; 1 < data.lengh; i++){
+            tr = '<tr>' +
+                    '<td>' + data[i].cod + '</td>' +
+                    '<td>' + data[i].nome + '</td>'+
+                    '<td>' + data[i].descri + '</td>'+
+                    '<td>' + data[i].qtda + '</td>'+
+                    '<td>' + data[i].fabricante + '</td>'+
+                    '<td>' + data[i].datahora + '</td>'+
+                '</tr>';
+            tbodyList.innerHTML += tr;
+            tr = tbodyList.childNodes;
+
+        }
+    });
+}
+
+listAll();
